616e7580ceaa57cb4c95b103990ea3d9ee3b45e0
jeremykstone0911 <61753461+jeremykstone0911@users.noreply.github.com>
2020-08-09 15:31:07 -0500

    completed time block structure in index.html

--- README.md
+++ README.md
@@ -1,33 +1,18 @@
 # Workday-Scheduler
 
-# 05 Homework - Using Third-Party APIs
+https://jeremykstone0911.github.io/Workday-Scheduler/
 
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+This program allows a user to plan out their day. They can save events into a particular time slot. When they refresh the page those events will stay there. The current day is displayed at the top of the page.
 
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+## Explanation
 
-## User Story
+There are 9 hour slots for the user to enter their activity for each hour. The HTML page has 9 rows, one for each time slot. Each row has 3 columns: the time slot, the event and a button to save the event. If the time has past the event is displayed as a gray box. If it is the current hour then, it is displayed as red. Lastly if it is in the future then the box is green. These colors are all updated by the script. When the user enters something into the event box and presses the save button that corresponds to that event, then the event is overwritten.
 
-```
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
+![Overview of the Daily Planner](assets/05-third-party-apis-homework-demo.gif)
 
-## Acceptance Criteria
+## Future Endevers
 
-```
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
+- Make a calendar, so that the user can update different days instead of just the current day
+- Add more specific time slots, so the user can update a more specified time
+- Add holidays to the calendar
+- Add reminders for the user
--- script.js
+++ script.js
@@ -1,37 +1,111 @@
-//Header displays current time
-// The current day//
-window.onload = function () {
-  var today = new Date();
-  date = moment().format("LL");
+// On ready function used to allow the html page to fully load before intiating updates to the DOM
+$(document).ready(function () {
+  // Create an array with the days of the week
+  let daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-  $("#currentDay").html(date);
-};
+  // Create an array with the months of the year
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-var workHours = [
-  "9AM",
-  "10AM",
-  "11AM",
-  "12PM",
-  "1PM",
-  "2PM",
-  "3PM",
-  "4PM",
-  "5PM",
-];
+  // Create a variable to define a new date
+  let date = new Date();
 
-//example code for setting up a timer
-//setinterval requires two parameters - function, and the time that you want it to run
-setInterval(function () {
-  //whatever you want to have run on a regularly scheduled time
-  //in this instance it's updating the currentDay div
-  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-  //then set how often you want the above code to run:
-}, 1000);
+  // Retrieve the day of the week from the array using the get day function for the index
+  let dayOfTheWeek = daysOfTheWeek[date.getDay()];
 
-//8 divs representing each hour from 9-5
+  // Retrieve the month of the year from the array using the get month function for the index
+  let month = months[date.getMonth()];
 
-//grey divs before the present; red divs for current hour; green divs for future
+  // Retrieve the date using the get date function
+  let day = date.getDate();
 
-//third column is save which saves to local storage
+  // Retrieve the year using the get year function
+  let year = date.getFullYear();
 
-//
+  // Concatenate the full date into a variable
+  let completeDate = dayOfTheWeek + ", " + month + " " + day + ", " + year;
+
+  // Select the paragraph with the CurrentDay id to insert the full date
+  $("#currentDay").text(completeDate);
+
+  // Set a variable equal to the current hour
+  let currentHour = date.getHours();
+
+  // Create an empty array to store inputs
+  let listOfInputs = [];
+
+  // Retrieve the userInputs key or else create an empty array
+  // The local storage is parsed into an object
+  let listOfInputsStored = JSON.parse(localStorage.getItem("userInputs")) || [];
+
+  // Create a for loop to iterate through the input tags
+  for (let i = 0; i < $("input").length; i++) {
+    // Define a saveIcon variable equal to selecting all the button elements in the HTML
+    let saveIcon = $("button");
+
+    // Add the icon to each button
+    saveIcon.addClass("fa fa-save");
+
+    // Select each input element
+    let elem = $("input")[i];
+
+    // Store the value of each element into local storage
+    $(elem).val(listOfInputsStored[i]);
+
+    // Push the value into the empty array
+    listOfInputs.push($(elem).val());
+
+    // Compare the current hour to to the time slot
+    // If it's the same, make the input box red
+    if (currentHour === Number($(elem).attr("time"))) {
+      $(elem).addClass("bg-danger");
+    }
+    // If the current hour is past the time slot, make the input box light grey
+    else if (currentHour > Number($(elem).attr("time"))) {
+      $(elem).addClass("bg-light");
+    }
+    // If it's before the time slot make the input box green
+    else if (currentHour < Number($(elem).attr("time"))) {
+      $(elem).addClass("bg-success");
+    }
+  }
+
+  // Select all buttons for an on click event
+  $("button").click(function () {
+    // Retrieve the data target input property and set it to buttonFor
+    // Each button corresponds to a specific input value to save
+    let buttonFor = $(this).data("targetinput");
+
+    // Find the position attribute of the specific button
+    let position = $(buttonFor).attr("position");
+
+    // Using the button clicked on get the the associated input
+    let inputVal = $(buttonFor).val();
+
+    // Set the value of the input to the specified position in the listOfInputs array
+    listOfInputs[position] = inputVal;
+
+    // Set the updated array into local storage
+    localStorage.setItem("userInputs", JSON.stringify(listOfInputs));
+  });
+});
