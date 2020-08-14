bf63582277919e1b6d01502608354f445efeaf43
jeremykstone0911 <61753461+jeremykstone0911@users.noreply.github.com>
2020-08-14 12:44:04 -0500

    rebuilt html and script

--- script.js
+++ script.js
@@ -1,111 +1,53 @@
-// On ready function used to allow the html page to fully load before intiating updates to the DOM
 $(document).ready(function () {
-  // Create an array with the days of the week
-  let daysOfTheWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+  // onclick for Save Button
+  $(".saveBtn").on("click", function () {
+    // What to save
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
-  // Create an array with the months of the year
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  // Create a variable to define a new date
-  let date = new Date();
-
-  // Retrieve the day of the week from the array using the get day function for the index
-  let dayOfTheWeek = daysOfTheWeek[date.getDay()];
-
-  // Retrieve the month of the year from the array using the get month function for the index
-  let month = months[date.getMonth()];
-
-  // Retrieve the date using the get date function
-  let day = date.getDate();
-
-  // Retrieve the year using the get year function
-  let year = date.getFullYear();
-
-  // Concatenate the full date into a variable
-  let completeDate = dayOfTheWeek + ", " + month + " " + day + ", " + year;
-
-  // Select the paragraph with the CurrentDay id to insert the full date
-  $("#currentDay").text(completeDate);
-
-  // Set a variable equal to the current hour
-  let currentHour = date.getHours();
-
-  // Create an empty array to store inputs
-  let listOfInputs = [];
-
-  // Retrieve the userInputs key or else create an empty array
-  // The local storage is parsed into an object
-  let listOfInputsStored = JSON.parse(localStorage.getItem("userInputs")) || [];
-
-  // Create a for loop to iterate through the input tags
-  for (let i = 0; i < $("input").length; i++) {
-    // Define a saveIcon variable equal to selecting all the button elements in the HTML
-    let saveIcon = $("button");
-
-    // Add the icon to each button
-    saveIcon.addClass("fa fa-save");
-
-    // Select each input element
-    let elem = $("input")[i];
-
-    // Store the value of each element into local storage
-    $(elem).val(listOfInputsStored[i]);
-
-    // Push the value into the empty array
-    listOfInputs.push($(elem).val());
-
-    // Compare the current hour to to the time slot
-    // If it's the same, make the input box red
-    if (currentHour === Number($(elem).attr("time"))) {
-      $(elem).addClass("bg-danger");
-    }
-    // If the current hour is past the time slot, make the input box light grey
-    else if (currentHour > Number($(elem).attr("time"))) {
-      $(elem).addClass("bg-light");
-    }
-    // If it's before the time slot make the input box green
-    else if (currentHour < Number($(elem).attr("time"))) {
-      $(elem).addClass("bg-success");
-    }
-  }
-
-  // Select all buttons for an on click event
-  $("button").click(function () {
-    // Retrieve the data target input property and set it to buttonFor
-    // Each button corresponds to a specific input value to save
-    let buttonFor = $(this).data("targetinput");
-
-    // Find the position attribute of the specific button
-    let position = $(buttonFor).attr("position");
-
-    // Using the button clicked on get the the associated input
-    let inputVal = $(buttonFor).val();
-
-    // Set the value of the input to the specified position in the listOfInputs array
-    listOfInputs[position] = inputVal;
-
-    // Set the updated array into local storage
-    localStorage.setItem("userInputs", JSON.stringify(listOfInputs));
+    // save to localStorage
+    localStorage.setItem(time, value);
   });
+
+  function updateBlock() {
+    // get current number of hours
+    var currentHour = moment().hours();
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      // check if we've moved past this time
+      //grey divs before the present; red divs for current hour; green divs for future
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+  // call function
+  updateBlock();
+
+  setInterval(updateBlock, 1000);
+  //whatever you want to have run on a regularly scheduled time
+  //in this instance it's updating the currentDay div
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:"));
+  //then set how often you want the above code to run:
+
+  //load saved data
+  $("#nine .description").val(localStorage.getItem("nine"));
+  $("#ten .description").val(localStorage.getItem("ten"));
+  $("#eleven .description").val(localStorage.getItem("eleven"));
+  $("#twelve .description").val(localStorage.getItem("twelve"));
+  $("#one .description").val(localStorage.getItem("one"));
+  $("#two .description").val(localStorage.getItem("two"));
+  $("#three .description").val(localStorage.getItem("three"));
+  $("#four .description").val(localStorage.getItem("four"));
+  $("#five .description").val(localStorage.getItem("five"));
 });
